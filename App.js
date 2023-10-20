488a11dbd5e2f963751077b8740aff51b6bbeda9
Alan Alves <alanalves071@gmail.com>
2023-10-20 11:40:50 -0300

    qrCode feito no android usando react native

--- App.js
+++ App.js
@@ -1,20 +1,57 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+
+import { StyleSheet, Text, View, Image } from 'react-native';
+import image from './assets/image-qr-code.png';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View  style={styles.containerBox}>
+            <Image source={image} style={styles.styleImg}/>
+            <View style={styles.textArea}>
+                <Text style={styles.textTitle}>
+                Improve your front-end skills by building projects  
+                </Text>
+                <Text style={styles.textSub}>
+                Scan the QR code to visit Frontend Mento and take your coding skills to the next level
+                </Text>
+            </View>
+        </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  container:{
+    backgroundColor: "#d6e1f0",
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
   },
+  containerBox: {
+  },
+  styleImg:{
+    width: 350,
+    height: 350,
+    borderRadius: 10,
+    alignSelf: 'center',
+    marginBottom: 100 ,
+
+  },
+  textTitle:{
+    fontWeight: 'bold',
+    fontSize: 25,
+    width: 350,
+    margin: 0,
+    color: "#383D4D",
+    textAlign: 'center',
+  },
+  textSub:{
+    width: 350,
+    margin: 0,
+    marginTop: 15,
+    color: '#A4A4A6',
+    textAlign: 'center',
+    fontSize: 18,
+  }
+  
 });
